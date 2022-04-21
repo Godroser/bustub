87506ef33af87ec89ca9d5d72f4cc750c6934335
Godroser <57433690+Godroser@users.noreply.github.com>
2022-04-21 19:35:32 +0800

    Add files via upload

--- src/buffer/clock_replacer.cpp
+++ src/buffer/clock_replacer.cpp
@@ -14,16 +14,49 @@
 
 namespace bustub {
 
-ClockReplacer::ClockReplacer(size_t num_pages) {}
+ClockReplacer::ClockReplacer(size_t num_pages) {
+    this->sizeframe=num_pages;
+}
 
 ClockReplacer::~ClockReplacer() = default;
 
-bool ClockReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool ClockReplacer::Victim(frame_id_t *frame_id) { 
+    if (l_frames.size()==0) return false;
+    else{
+        for (std::list<frame_id_t>::iterator it = l_frames.begin(); it != l_frames.end(); it++){
+            if (*frame_id==*it) {
+                l_frames.remove(*it);
+                return true;
+            }
+        }
+    }
+    return false;
 
-void ClockReplacer::Pin(frame_id_t frame_id) {}
+}
 
-void ClockReplacer::Unpin(frame_id_t frame_id) {}
+void ClockReplacer::Pin(frame_id_t frame_id) {
+    for (std::list<frame_id_t>::iterator it = l_frames.begin(); it != l_frames.end(); it++){
+        if (frame_id==*it) {
+            l_frames.remove(*it);
+            return;
+        }
+    }
+}
 
-size_t ClockReplacer::Size() { return 0; }
+void ClockReplacer::Unpin(frame_id_t frame_id) {
+    if (l_frames.size()<this->sizeframe){
+        l_frames.push_back(frame_id);
+    }
+    else{
+        l_frames.pop_front();
+        l_frames.push_back(frame_id);
+    }
+    
+    return;
+}
+
+size_t ClockReplacer::Size() {
+    return this->sizeframe; 
+    }
 
 }  // namespace bustub
--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -18,12 +18,42 @@ LRUReplacer::LRUReplacer(size_t num_pages) {}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+    if (l_frames.size()==0) return false;
+    else{
+        for (std::list<frame_id_t>::iterator it = l_frames.begin(); it != l_frames.end(); it++){
+            if (*frame_id==*it) {
+                l_frames.remove(*it);
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    for (std::list<frame_id_t>::iterator it = l_frames.begin(); it != l_frames.end(); it++){
+        if (frame_id==*it) {
+            l_frames.remove(*it);
+            return;
+        }
+    }
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    if (l_frames.size()<this->sizeframe){
+        l_frames.push_back(frame_id);
+    }
+    else{
+        l_frames.pop_front();
+        l_frames.push_back(frame_id);
+    }
+    
+    return;
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() {
+    return this->sizeframe; 
+}
 
 }  // namespace bustub
